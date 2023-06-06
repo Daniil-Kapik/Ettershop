6356942571d4eb10668920389506535259934d4f
Daniil-Kapik <dann_kap@mail.ru>
2023-06-06 16:22:00 +0300

    correct lesson_3

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from mainapp.models import ProductCategory, Product
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'price')
+
+
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
